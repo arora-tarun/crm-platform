145e3b41674b68ccbdc688a002643cde98e584ef
arora-tarun <aroratanu552004@gmail.com>
2025-06-03 01:12:03 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/auth/failure' }),
   (req, res) => {
-    res.redirect('https://crm-platform-s759.onrender.com/auth/google/callback');
+    res.redirect('https://crm-frontend-s759.onrender.com/dashboard');
   }
 );
 
@@ -23,7 +23,7 @@ router.get('/user', (req, res) => {
 router.get('/logout', (req, res, next) => {
   req.logout(function(err) {
     if (err) return next(err);
-    res.redirect('https://crm-frontend-s759.onrender.com/auth/google/callback');
+    res.redirect('https://crm-frontend-s759.onrender.com/');
   });
 });
 
