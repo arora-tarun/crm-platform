b48d034f8e4bff4764ae8e971c58af6e0929f742
arora-tarun <aroratanu552004@gmail.com>
2025-06-03 02:09:35 +0530

    Delete crm-frontend/src/api.js

--- crm-frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  withCredentials: true, // if you're using cookies for authentication
-});
-
-export default api;
