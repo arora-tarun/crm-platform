7f4867d36a116ff492300795d644b209c432937b
arora-tarun <aroratanu552004@gmail.com>
2025-06-03 00:20:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Configure CORS to allow frontend origin and allow credentials
 app.use(cors({
-  origin: 'http://localhost:3000',   // React frontend URL
+  origin: 'https://crm-frontend-s759.onrender.com',   // React frontend URL
   credentials: true,                 // Allow cookies to be sent cross-origin
 }));
 
