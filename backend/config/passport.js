4a371122fa9dfce0dbbb762f65d02197fe75ffa2
arora-tarun <aroratanu552004@gmail.com>
2025-06-03 10:48:11 +0530

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL:https://crm-platform-s759.onrender.com/auth/google/callback,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
